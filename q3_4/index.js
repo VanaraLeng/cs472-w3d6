1c9e5d13c4138eba6da9b3a14a93267368509a0c
vanara.leng <vanara.leng@asiacell.com>
2023-02-06 23:06:17 -0600

    Update logic
    Add product to inventory

--- q3_4/index.js
+++ q3_4/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bparser = require('body-parser');
-const Product = require("./Product");
+const product = require("./model/product");
+const inventory = require("./model/inventory");
+
+const cart = [];
 
 app.use(bparser.urlencoded());
 
@@ -10,33 +13,35 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'view'));
 app.use('/css', express.static(path.join(__dirname, "public", "stylesheets")));
 
-// Hard-coded product
-const products = [];
-
-const product = new Product(12, "Macbook Pro M2", 3.5, "Apple Laptop")
 
 app.get('/', (req, res) => {  
   res.render('product', {
-    product : product
+    products : inventory
   });
 });
 
-app.post('/addToCart', (req, res) => {
+app.post('/addToInventory', (req, res) => {
   const id = req.body.id;
   const name = req.body.name;
   const price = req.body.price;
   const description = req.body.description
-  const product = new Product(id, name, price, description)
-
-  products.push(product);
-  console.log(products);
+  const prod = new product(id, name, price, description)
+  inventory.push(prod);
   
+  res.redirect('/')
+});
+
+
+app.post('/addToCart', (req, res) => {
+  const id = parseInt(req.body.id);
+  const prod = inventory.filter(x => x.id === id)[0];
+  cart.push(prod);
   res.redirect('/shoppingcart')
 });
 
 app.get('/shoppingcart', (req, res) => {
   res.render('shoppingcart', {
-    products : products
+    products : cart
   })
 });
 
